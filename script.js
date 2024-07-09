d3e2fbc054776ac7408000f0fbaad3a898de7580
AishwaryaKandasamy <15aish98@gmail.com>
2024-07-09 19:38:27 +0530

    portfolio-updates

--- script.js
+++ script.js
@@ -1,54 +1,22 @@
-// JavaScript code for interactive elements
+document.addEventListener('DOMContentLoaded', () => {
+    function loadContent(url, containerId) {
+        fetch('sections/' + url)
+            .then(response => response.text())
+            .then(html => {
+                document.getElementById(containerId).innerHTML = html;
+            })
+            .catch(error => console.error('Error loading content:', error));
+    }
 
-// Smooth scrolling function
-function smoothScroll(target) {
-    document.querySelector(target).scrollIntoView({
-        behavior: 'smooth'
-    });
-}
+    // Load projects section
+    loadContent('projects.html', 'projects-container');
 
-// Navigation links
-const aboutLink = document.getElementById('about-link');
-const resumeLink = document.getElementById('resume-link');
-const projectsLink = document.getElementById('projects-link');
-const contactLink = document.getElementById('contact-link');
+    // Load experiences section
+    loadContent('experiences.html', 'experiences-container');
 
-aboutLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    smoothScroll('#about');
-});
+    // Load technical skills section
+    loadContent('technical_skills.html', 'technical-skills-container');
 
-resumeLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    smoothScroll('#resume');
-});
-
-projectsLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    smoothScroll('#projects');
-});
-
-contactLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    smoothScroll('#contact');
-});
-
-// Button clicks
-const resumeButton = document.getElementById('resume-button');
-const projectsButton = document.getElementById('projects-button');
-
-resumeButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    // You can update content or perform an action related to Resume
-    document.getElementById('paragraph1').textContent = "This is my resume content.";
-    document.getElementById('paragraph2').textContent = "Feel free to contact me for more details.";
-    smoothScroll('#resume');
-});
-
-projectsButton.addEventListener('click', function(event) {
-    event.preventDefault();
-    // You can update content or perform an action related to Projects
-    document.getElementById('paragraph1').textContent = "These are some of my projects.";
-    document.getElementById('paragraph2').textContent = "Explore them further to see my work.";
-    smoothScroll('#projects');
+    // Load contact section
+    loadContent('contact.html', 'contact-container');
 });
