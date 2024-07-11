a1859fb68d2505cf536a5ee8e6e8506ae3373fa4
AishwaryaKandasamy <15aish98@gmail.com>
2024-07-11 20:45:29 +0530

    portfolio-updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", function() {
     function loadContent(url, containerId) {
         fetch('sections/' + url)
             .then(response => response.text())
